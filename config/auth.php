63edf29e5513f6933a1d46c7284fb21100709683
Sachintha De Silva <yslaksura@gmail.com>
2026-04-26 10:39:05 +0530

    fix: default to InMemoryTokenStorage in skeleton

--- config/auth.php
+++ config/auth.php
@@ -1,10 +1,12 @@
 <?php
 declare(strict_types=1);
 use Vortos\Auth\DependencyInjection\VortosAuthConfig;
+use Vortos\Auth\Storage\InMemoryTokenStorage;
 
 return static function (VortosAuthConfig $config): void {
     $config
         ->secret($_ENV['JWT_SECRET'] ?? 'changeme')
         ->accessTokenTtl((int)($_ENV['JWT_ACCESS_TTL'] ?? 900))
-        ->refreshTokenTtl((int)($_ENV['JWT_REFRESH_TTL'] ?? 604800));
+        ->refreshTokenTtl((int)($_ENV['JWT_REFRESH_TTL'] ?? 604800))
+        ->tokenStorage(InMemoryTokenStorage::class);
 };

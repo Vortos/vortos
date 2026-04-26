ac0c582fdba06aa75066a00bfe86704fc2734814
Sachintha De Silva <yslaksura@gmail.com>
2026-04-26 08:16:16 +0530

    chore: remove docker from skeleton, fix index.php, add env vars

--- public/index.php
+++ public/index.php
@@ -6,10 +6,19 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Vortos\Foundation\Runner;
 
 $config = require __DIR__ . '/../bootstrap/app.php';
-$runner = new Runner(...$config, context: 'http');
-$kernel = $runner->getKernel();
 
-$request = Symfony\Component\HttpFoundation\Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+if (isset($_SERVER['FRANKENPHP_WORKER'])) {
+    $runner = new Runner(...$config, context: 'http');
+    $runner->getContainer();
+    while (frankenphp_handle_request(function () use ($runner): void {
+        header('X-Vortos-Mode: Worker-Active');
+        $response = $runner->run();
+        $response->send();
+        $runner->cleanUp();
+    })) {}
+} else {
+    $runner = new Runner(...$config, context: 'http');
+    $response = $runner->run();
+    $response->send();
+    $runner->cleanUp();
+}

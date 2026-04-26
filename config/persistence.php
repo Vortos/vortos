f7fc82955d00911ef57321a4e69a5d8446b51b25
Sachintha De Silva <yslaksura@gmail.com>
2026-04-26 10:31:38 +0530

    fix: correct persistence config API

--- config/persistence.php
+++ config/persistence.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 use Vortos\Persistence\DependencyInjection\VortosPersistenceConfig;
 
 return static function (VortosPersistenceConfig $config): void {
-    $config->write()->dsn($_ENV['DB_DSN'] ?? '');
-    $config->read()->dsn($_ENV['MONGO_URI'] ?? '')->database($_ENV['MONGO_DB'] ?? '');
+    $config
+        ->writeDsn($_ENV['DB_DSN'] ?? '')
+        ->readDsn($_ENV['MONGO_URI'] ?? '')
+        ->readDatabase($_ENV['MONGO_DB'] ?? '');
 };

83a5658424c747d5e1e3db860c8b38676c2609d5
Sachintha De Silva <yslaksura@gmail.com>
2026-05-01 00:49:26 +0530

    fix(skeleton): remove redundant POSTGRES_DB_NAME, use POSTGRES_DB only

--- config/persistence.php
+++ config/persistence.php
@@ -9,16 +9,16 @@ return static function (VortosPersistenceConfig $config): void {
         ->writeDsn($_ENV['DATABASE_URL'] ?? sprintf(
             'pgsql://%s:%s@%s:5432/%s',
             $_ENV['POSTGRES_USER'] ?? 'postgres',
-            $_ENV['POSTGRES_PASSWORD'] ?? 'postgres',
+            $_ENV['POSTGRES_PASSWORD'] ?? 'secret',
             $_ENV['POSTGRES_HOST'] ?? 'write_db',
-            $_ENV['POSTGRES_DB_NAME'] ?? 'app',
+            $_ENV['POSTGRES_DB'] ?? 'myapp',
         ))
         ->readDsn(sprintf(
             'mongodb://%s:%s@%s:%s',
             $_ENV['MONGO_INITDB_ROOT_USERNAME'] ?? 'root',
-            $_ENV['MONGO_INITDB_ROOT_PASSWORD'] ?? 'root',
+            $_ENV['MONGO_INITDB_ROOT_PASSWORD'] ?? 'secret',
             $_ENV['MONGO_HOST'] ?? 'read_db',
             $_ENV['MONGO_PORT'] ?? '27017',
         ))
-        ->readDatabase($_ENV['MONGO_DB_NAME'] ?? 'app');
+        ->readDatabase($_ENV['MONGO_DB_NAME'] ?? 'myapp');
 };

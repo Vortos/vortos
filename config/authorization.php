24879bb7d75f8dcc8dd345375982577401a76238
Sachintha De Silva <yslaksura@gmail.com>
2026-05-10 23:54:23 +0530

    removed config files from skeleton because now setup script generate them

--- config/authorization.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-use Vortos\Authorization\DependencyInjection\VortosAuthorizationConfig;
-
-return static function (VortosAuthorizationConfig $config): void {
-    $config->roleHierarchy([
-        'ROLE_ADMIN' => ['ROLE_USER'],
-    ]);
-};
